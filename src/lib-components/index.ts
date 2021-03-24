cd388a9502f98b709c5e7ea1a8c52692b6b2a539
Valentinos Galanos <sonidelav@hotmail.com>
2021-03-24 17:53:14 +0200

    added text editable control

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as VScroller } from './controls/VScroller.vue';
+export { default as TextEditable } from './controls/TextEditable.vue';
